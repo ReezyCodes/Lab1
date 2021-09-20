74a8a9f6ea8f566e3cb6425553eecdc1596c9e05
raulale@student.ubc.ca <raulale@student.ubc.ca>
2021-09-19 20:20:36 -0700

    Added Exercise 4

---
+++ Lab1/Exercise4.1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Exercise4._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxBytesToRead = new System.Windows.Forms.TextBox();
+            this.textBoxTempStringLength = new System.Windows.Forms.TextBox();
+            this.textBoxItemsQueue = new System.Windows.Forms.TextBox();
+            this.serialButton = new System.Windows.Forms.Button();
+            this.textBoxSerialDataStream = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBoxBytesToRead
+            // 
+            this.textBoxBytesToRead.Location = new System.Drawing.Point(162, 62);
+            this.textBoxBytesToRead.Name = "textBoxBytesToRead";
+            this.textBoxBytesToRead.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBytesToRead.TabIndex = 1;
+            // 
+            // textBoxTempStringLength
+            // 
+            this.textBoxTempStringLength.Location = new System.Drawing.Point(162, 88);
+            this.textBoxTempStringLength.Name = "textBoxTempStringLength";
+            this.textBoxTempStringLength.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTempStringLength.TabIndex = 2;
+            // 
+            // textBoxItemsQueue
+            // 
+            this.textBoxItemsQueue.Location = new System.Drawing.Point(162, 114);
+            this.textBoxItemsQueue.Name = "textBoxItemsQueue";
+            this.textBoxItemsQueue.Size = new System.Drawing.Size(100, 20);
+            this.textBoxItemsQueue.TabIndex = 3;
+            // 
+            // serialButton
+            // 
+            this.serialButton.Location = new System.Drawing.Point(162, 14);
+            this.serialButton.Name = "serialButton";
+            this.serialButton.Size = new System.Drawing.Size(122, 23);
+            this.serialButton.TabIndex = 4;
+            this.serialButton.Text = "Connect Serial";
+            this.serialButton.UseVisualStyleBackColor = true;
+            this.serialButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSerialDataStream
+            // 
+            this.textBoxSerialDataStream.Location = new System.Drawing.Point(12, 218);
+            this.textBoxSerialDataStream.Multiline = true;
+            this.textBoxSerialDataStream.Name = "textBoxSerialDataStream";
+            this.textBoxSerialDataStream.Size = new System.Drawing.Size(298, 220);
+            this.textBoxSerialDataStream.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Serial Bytes to Read";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Temp String Length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Items in Queue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Serial Data Stream";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 14);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(130, 17);
+            this.listBox1.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSerialDataStream);
+            this.Controls.Add(this.serialButton);
+            this.Controls.Add(this.textBoxItemsQueue);
+            this.Controls.Add(this.textBoxTempStringLength);
+            this.Controls.Add(this.textBoxBytesToRead);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxBytesToRead;
+        private System.Windows.Forms.TextBox textBoxTempStringLength;
+        private System.Windows.Forms.TextBox textBoxItemsQueue;
+        private System.Windows.Forms.Button serialButton;
+        private System.Windows.Forms.TextBox textBoxSerialDataStream;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab1/Exercise4.1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Collections.Concurrent;
+
+namespace Exercise4._1
+{
+    public partial class Form1 : Form
+    {
+        string serialDataString;
+        SerialPort _serialPort = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+        ConcurrentQueue<Int32> dataQueue = new ConcurrentQueue<Int32>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            if (listBox1.Items.Count == 0)
+                listBox1.Text = "No COM ports!";
+            else
+                listBox1.SelectedIndex = 0;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!(_serialPort.IsOpen))
+            {
+                _serialPort.Open();
+                _serialPort.Handshake = Handshake.None;
+                _serialPort.Write("A");
+                serialButton.Text = "Disconnect Serial";
+            }
+            else if (_serialPort.IsOpen)
+            {
+                _serialPort.Close();
+                serialButton.Text = "Connect Serial";
+            }
+            this.timer1.Enabled = true;
+        }
+
+        private void serialPort_DataReceived()
+        {
+            int newByte;
+            int bytesToRead;
+            bytesToRead = _serialPort.BytesToRead;
+            while (bytesToRead != 0)
+            {
+                newByte = _serialPort.ReadByte();
+                serialDataString = serialDataString + newByte.ToString() + ", ";
+                dataQueue.Enqueue(Convert.ToInt32(newByte));
+                bytesToRead = _serialPort.BytesToRead;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_serialPort.IsOpen)
+            {
+                serialPort_DataReceived();
+                textBoxBytesToRead.Text = _serialPort.BytesToRead.ToString();
+                textBoxTempStringLength.Text = serialDataString.Length.ToString();
+                textBoxItemsQueue.Text = dataQueue.Count.ToString();
+                serialDataString = "";
+                int byteOut;
+                int bytesToRead;
+                bytesToRead = _serialPort.BytesToRead;
+                while (dataQueue.TryDequeue(out byteOut) == true)
+                {
+                    textBoxSerialDataStream.AppendText(byteOut.ToString() + ", ");
+                }
+            }
+        }
+    }
+}
