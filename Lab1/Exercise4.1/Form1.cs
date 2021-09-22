d8fea53fe7e4ef1eb0070d5f79f2420938d3c4b4
raulale@student.ubc.ca <raulale@student.ubc.ca>
2021-09-22 10:31:05 -0700

    Fixed

--- Lab1/Exercise4.1/Form1.cs
+++ Lab1/Exercise4.1/Form1.cs
@@ -14,7 +14,7 @@ namespace Exercise4._1
 {
     public partial class Form1 : Form
     {
-        string serialDataString;
+        string serialDataString = "";
         SerialPort _serialPort = new SerialPort();
         ConcurrentQueue<Int32> dataQueue = new ConcurrentQueue<Int32>();
 
